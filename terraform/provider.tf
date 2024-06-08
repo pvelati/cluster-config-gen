fb9dbe1d674ca451877bc29b2b151c4f5fb27a4a
Paolo Velati <paolo.velati@gmail.com>
2024-06-08 22:46:11 +0000

    altri file che avevo dimenticato di copiare

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,5 +7,5 @@ provider "proxmox" {
   pm_api_url          = var.proxmox.api_url
   pm_api_token_id     = var.proxmox.api_token_id
   pm_api_token_secret = var.proxmox.api_token_secret
-  // pm_tls_insecure     = false
+  pm_tls_insecure     = false
 }
