4870ff64389e1229f03ce16fdb11ed74aafac853
Paolo Velati <paolo.velati@gmail.com>
2024-06-08 22:36:38 +0000

    aggiunta la parte terraform copiata da un altro repo che avevo

---
+++ terraform/README.md
@@ -0,0 +1,15 @@
+## Installation
+
+Copy age-key.txt inside repo folder
+
+```bash
+  make decrypt
+```
+    
+## Deployment
+
+```bash
+  make init
+  make plan
+  make apply
+```
---
+++ terraform/provider.tf
@@ -0,0 +1,11 @@
+provider "opnsense" {
+  uri = var.opnsense.uri 
+  api_key = var.opnsense.api_key
+  api_secret = var.opnsense.api_secret
+}
+provider "proxmox" {
+  pm_api_url          = var.proxmox.api_url
+  pm_api_token_id     = var.proxmox.api_token_id
+  pm_api_token_secret = var.proxmox.api_token_secret
+  // pm_tls_insecure     = false
+}
